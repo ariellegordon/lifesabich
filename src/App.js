0262276775acc7df49fcf052921328de0ee4e5e8
Arielle Gordon <reallygordon@gmail.com>
2018-09-18 22:53:20 -0400

    more stuff is working

--- src/App.js
+++ src/App.js
@@ -12,17 +12,54 @@ class App extends Component {
       displayMenu: false
     };
     this.handleWindowHover = this.handleWindowHover.bind(this);
+    this.showAbout = this.showAbout.bind(this);
+    this.removeAbout = this.removeAbout.bind(this);
   }
 
   handleWindowHover(evt) {
     const { target } = evt;
     target.classList.add("expand");
+    let aboutSabich = document.querySelector(".about-sabich");
+    aboutSabich.classList.add("display-menu");
+  }
 
-    // let modal = document.querySelector(".modal-container");
-    // modal.classList.add("active");
+  showAbout() {
+    console.log("Clicked Show About");
+    let aboutInner = document.querySelector(".about-inner");
+    aboutInner.classList.toggle("display-about");
+  }
+
+  removeAbout() {
+    console.log("Clicked Remove About");
+    let aboutInner = document.querySelector(".about-inner");
+    aboutInner.classList.remove("display-about");
   }
 
   render() {
+    const aboutData = `LIFESABICH is a food truck that doesn’t exist. A
+              delicious sandwich, teetering on the edge of “rare,” that is just
+              around the corner. It’s your special spot for which you want
+              credit for discovery but relish the anonymity. A tasty snack you
+              can never find. It is a zine, from the front lines of fashion
+              examining the origins of desire. We’ve outsmarted our diets, or at
+              least tried to, with endless attempts at nourishing, starving,
+              restricting, and binging, constantly trying to satiate an
+              unattainable and unnatural homeostasis. Often giving into what we
+              don’t even know we crave—endless feeds of tuna tostadas, bulbous
+              spirulina tinted smoothies, team-less sportswear, the ugliest
+              sneakers you’ve ever seen—how do we take it all in, even when our
+              stomachs and wallets tell us we’re full? LIFESABICH exists to ask:
+              what does it mean to tell the story of a product that isn’t real?
+              Does desire need to be plastic? An invitation: LIFESABICH calls a
+              reader to accept that there is no food truck, but that as a
+              participant they can chase it down regardless. After all, isn’t
+              that what fashion is all about? Running after a collective
+              fantasy, some- times so fast we choose to ignore our shoe is
+              untied, where your speed speaks to your allegiance. Founded by a
+              Designer and a Buyer, LIFESABICH is made by two people who love
+              fashion but also want to foster a forum to speak both candidly and
+              critically. By pursuing print, rather than digital, the intention
+              is for a slower conversation, outside of quick trends.`;
     return (
       <div>
         <div>
@@ -37,6 +74,24 @@ class App extends Component {
                   style={{ backgroundColor: "#ffc5c5" }}
                   onMouseOver={this.handleWindowHover}
                 />
+                <div className="about-sabich">
+                  <div className="about">
+                    <div onClick={this.showAbout}>
+                      <h3 style={{ fontFamily: "SabonBol" }}>About</h3>
+                    </div>
+                    <div className="about-inner">
+                      <div className="about-x-button" onClick={this.showAbout}>
+                        <h3 style={{ fontFamily: "SabonBol" }}>x</h3>
+                      </div>
+                      <h3 style={{ fontFamily: "SabonBol", fontSize: "20px" }}>
+                        {aboutData}
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="sabich">
+                    <h3 style={{ fontFamily: "SabonBol" }}>What is Sabich?</h3>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
